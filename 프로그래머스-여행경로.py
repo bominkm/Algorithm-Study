ca1c3833393d14ee9d5395abb994cd4ffc442946
bominkm <68378332+bominkm@users.noreply.github.com>
2021-09-05 00:50:52 +0900

    Update 프로그래머스-여행경로.py

--- 프로그래머스-여행경로.py
+++ 프로그래머스-여행경로.py
@@ -1,3 +1,27 @@
+## 정답 풀이 ##
+def solution(tickets):
+    # 출발지: key, 도착지: value인 딕셔너리
+    tickets.sort(reverse=True)
+    routes = dict()
+    for t1, t2 in tickets:
+        # 이미 있는 경우 
+        if t1 in routes:
+            routes[t1].append(t2)
+        else:
+            routes[t1] = [t2]
+    st = ['ICN']
+    ans = []
+    # DFS
+    while st:
+        top = st[-1]
+        # 더 이상 갈 수 없는 공항 -> 도착지
+        if top not in routes or len(routes[top])==0:
+            ans.append(st.pop())
+        else:
+            st.append(routes[top].pop())
+    ans.reverse()
+    return ans
+
 ## 틀린 풀이 ##
 # 알파벳 순으로 방문하면 모든 곳을 방문하지 못할 수 있음 -> BFS
 def solution(tickets):
